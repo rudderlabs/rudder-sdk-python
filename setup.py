87c49c9811f9c90173ba7bf33722a58b3f0554f1
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2024-06-26 23:00:30 +0530

    fix: vulnerable dependencies and clean up

--- setup.py
+++ setup.py
@@ -1,47 +1,34 @@
-import os
-import sys
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+from pathlib import Path
 
 # Don't import rudder_analytics module here, since deps may not be installed
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'rudderstack', 'analytics'))
+sys.path.insert(0, str(Path(__file__).resolve().parent / 'rudderstack' / 'analytics'))
 from rudderstack.analytics.version import VERSION
 
-long_description = '''
-RudderStack is a platform for collecting, storing and routing customer event data to dozens
-of tools. RudderStack is open-source, can run in your cloud environment
-(AWS, GCP, Azure or even your data-centre) and provides a powerful transformation
-framework to process your event data on the fly.
-'''
+long_description = (Path(__file__).resolve().parent / 'README.md').read_text(encoding='utf-8')
 
 install_requires = [
-    "requests>=2.8,<3.0",
-    "monotonic>=1.5,<2.0",
-    "backoff>=2.1,<3.0",
-    "python-dateutil>=2.2,<3.0",
-    "python-dotenv>=0.21.0,<2.0.0",
-    "deprecation>=2.0.6,<3.0.0",
+    "requests>=2.32.3,<3.0",
+    "monotonic>=1.6,<2.0",
+    "backoff>=2.2.1,<3.0",
+    "deprecation>=2.1.0,<3.0.0",
 ]
 
 tests_require = [
-    "mock==2.0.0",
-    "pylint==2.8.0",
-    "flake8==3.7.9",
+    "mock==4.0.3",
+    "flake8==7.1.0",
 ]
 
 setup(
     name='rudder-sdk-python',
     version=VERSION,
     url='https://github.com/rudderlabs/rudder-sdk-python',
-    author='Rudderstack',
+    author='RudderStack',
     author_email='sdk@rudderstack.com',
-    maintainer='Rudderstack',
+    maintainer='RudderStack',
     maintainer_email='sdk@rudderstack.com',
     test_suite='rudderstack.analytics.test.all',
-    packages=['rudderstack.analytics', 'rudderstack.analytics.test'],
+    packages=find_packages(exclude=["tests*"]),
     python_requires='>=3.6.0',
     license='MIT License',
     install_requires=install_requires,
@@ -51,15 +38,23 @@ setup(
     },
     description='RudderStack is an open-source Segment alternative written in Go, built for the enterprise.',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    zip_safe=False,
+    include_package_data=True,
 )
